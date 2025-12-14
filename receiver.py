720a8bcdcf5fd2c59f1a9902a938c2bc86480b93
ghost-sakura <s34463201@gmail.com>
2025-12-15 01:34:57 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,7 +1,7 @@
 from vidstream import StreamingServer
 import threading
 
-receiver = StreamingServer('192.168.1.8',9999)
+receiver = StreamingServer('receiver ip address',9999)
 
 t = threading.Thread(target=receiver.start_server)
 t.start()
